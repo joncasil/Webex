91f91e2d9fc81d01d628ca3781cd65270ecf629d
joncasil <74035570+joncasil@users.noreply.github.com>
2020-11-08 20:02:54 -0600

    API call for user info
    API call for user info

---
+++ webex.py
@@ -0,0 +1,8 @@
+import json
+import requests
+from pprint import pprint
+r = requests.get('https://webexapis.com/v1/people/me',headers={'Authorization': 'Bearer YzhiYTBlYTctMTVmNS00YTBjLWJjY2EtMDFiNWY4MzVkMmNiMWZjMjNiNWQtNWJk_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f', 'accept': 'application/json'})
+
+json_text= json.loads(r.text)
+
+pprint(json_text) 
